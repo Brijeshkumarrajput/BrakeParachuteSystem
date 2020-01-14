cd4c0950e08505f41c467ad1071abafbc2c303b6
Brijesh Kumar <brijeshrajpt@gmail.com>
2020-01-15 02:16:27 +0530

    shock

---
+++ simulator/opening_shock.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Opening Shock Calculator</title>
+  </head>
+  <body>
+    <h1>THIS IS THE OPENING SHOCK CALCULATOR</h1>
+  </body>
+</html>
